b472706a16535776c08819cc1615498fa904d0fd
ngocphat922004 <130296645+ngocphat922004@users.noreply.github.com>
2024-10-30 00:05:52 +0700

    Myorder_complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,21 @@
-// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'src/Screen/helloWorld.dart'; // Import Màn hình mọi người đang test nha
+import 'src/Screen/helloWorld.dart'; // Import Màn hình mọi người đang test
+import 'src/Screen//MyOrder//Ongoing_complete/Ongoing.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-  await EasyLocalization.ensureInitialized();
 
-  runApp(
-    EasyLocalization(
-      supportedLocales: [Locale('en'), Locale('vi')],
-      path: 'assets/translations', // Đường dẫn tới file ngôn ngữ
-      fallbackLocale: Locale('en'),
-      child: MyApp(),
-    ),
-  );
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      home: HelloWorld(), // Gọi màn hình mọi người đang code vào đây nha
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Ongoing(), // Gọi màn hình Ongoing vào đây
     );
   }
 }
