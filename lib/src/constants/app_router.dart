afcd24e763b30aa10de712325685f87275d26787
minhnhut2306 <thanhtcps33354@fpt.edu.vn>
2024-11-02 00:32:17 +0700

    done login register onboarding

---
+++ lib/src/constants/app_router.dart
@@ -0,0 +1,35 @@
+import 'package:beauty/src/widget/Onboarding%20-%20Start.dart';
+import 'package:beauty/src/widget/Onboarding.dart';
+import 'package:beauty/src/widget/Signin.dart';
+import 'package:beauty/src/widget/Signup.dart';
+import 'package:beauty/src/widget/splash_widget.dart';
+import 'package:go_router/go_router.dart';
+
+class MyAppRouter {
+  GoRouter router = GoRouter(
+    initialLocation: "/splash",
+    routes: [
+      GoRoute(
+        name: "Splash",
+        path: '/splash',
+        builder: (context, state) => SplashWidget(),
+      ),
+      GoRoute(
+          path: '/onnboardingstart',
+          name: 'OnboardingStartScreen',
+          builder: (context, state) => OnboardingStartScreen()),
+      GoRoute(
+          path: '/onnboarding',
+          name: 'OnboardingScreen',
+          builder: (context, state) => OnboardingScreen()),
+      GoRoute(
+          path: '/signup',
+          name: 'SignupScreen',
+          builder: (context, state) => SignupScreen()),
+      GoRoute(
+          path: '/signin',
+          name: 'SigninScreen',
+          builder: (context, state) => SigninScreen()),
+    ],
+  );
+}
--- lib/src/qrcode.dart
+++ lib/src/qrcode.dart
@@ -1,2 +1,4 @@
+// ignore: unused_import
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:qr_code_scanner/qr_code_scanner.dart';
