5abc9a261767cf93616c335d7c56aeda22f66ae8
thungan610 <156633890+thungan610@users.noreply.github.com>
2024-11-03 23:10:01 +0700

    HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // import 'package:firebase_core/firebase_core.dart';
+import 'package:beauty/src/Screen/HomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'src/Screen/helloWorld.dart'; // Import Màn hình mọi người đang test nha
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       locale: context.locale,
       // home: HelloWorld(), // Gọi màn hình mọi người đang code vào đây nha
       // home: shop(),
-      home: shopEmpty(), 
+      home: const HomeScreen(), 
 
     );
   }
