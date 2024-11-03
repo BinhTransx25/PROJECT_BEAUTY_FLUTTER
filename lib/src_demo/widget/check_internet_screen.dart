d95cbdfbb0dcd0388fd27f924ab595754720d26e
Binh <binhthaps36193@fpt.edu.vn>
2024-11-03 14:06:26 +0700

    check

--- lib/src_demo/widget/check_internet_screen.dart
+++ lib/src_demo/widget/check_internet_screen.dart
@@ -1,17 +1,10 @@
 import 'dart:async';
 import 'dart:developer' as developer;
-<<<<<<<< HEAD:lib/src_demo/widget/check_internet_screen.dart
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'loadingscreen2.dart'; // Import Loadingscreen2
 import 'notfindscrren.dart'; // Import NotFoundScreen
-========
-
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
->>>>>>>> hung-dev:lib/checkinternet.dart
 
 class CheckInternet extends StatefulWidget {
   const CheckInternet({Key? key}) : super(key: key);
@@ -30,10 +23,7 @@ class _CheckInternetState extends State<CheckInternet> {
     super.initState();
     initConnectivity();
 
-<<<<<<<< HEAD:lib/src_demo/widget/check_internet_screen.dart
     // Lắng nghe thay đổi kết nối và cập nhật trạng thái
-========
->>>>>>>> hung-dev:lib/checkinternet.dart
     _connectivitySubscription =
         _connectivity.onConnectivityChanged.listen(_updateConnectionStatus as void Function(List<ConnectivityResult> event)?) as StreamSubscription<ConnectivityResult>;
   }
@@ -44,15 +34,9 @@ class _CheckInternetState extends State<CheckInternet> {
     super.dispose();
   }
 
-<<<<<<<< HEAD:lib/src_demo/widget/check_internet_screen.dart
   // Hàm khởi tạo kết nối để kiểm tra trạng thái ban đầu
   Future<void> initConnectivity() async {
     late ConnectivityResult result;
-========
-  Future<void> initConnectivity() async {
-    late ConnectivityResult result;
-
->>>>>>>> hung-dev:lib/checkinternet.dart
     try {
       result = (await _connectivity.checkConnectivity()) as ConnectivityResult;
     } on PlatformException catch (e) {
@@ -61,7 +45,6 @@ class _CheckInternetState extends State<CheckInternet> {
     }
 
     if (!mounted) {
-<<<<<<<< HEAD:lib/src_demo/widget/check_internet_screen.dart
       return;
     }
 
@@ -91,20 +74,6 @@ class _CheckInternetState extends State<CheckInternet> {
   }
 
   // Lấy thông báo trạng thái kết nối
-========
-      return Future.value(null);
-    }
-
-    return _updateConnectionStatus(result);
-  }
-
-  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-    setState(() {
-      _connectionStatus = result;
-    });
-  }
-
->>>>>>>> hung-dev:lib/checkinternet.dart
   String getConnectStatusMess(ConnectivityResult result) {
     switch (result) {
       case ConnectivityResult.wifi:
@@ -112,15 +81,9 @@ class _CheckInternetState extends State<CheckInternet> {
       case ConnectivityResult.mobile:
         return "Đang kết nối với mobile";
       case ConnectivityResult.none:
-<<<<<<<< HEAD:lib/src_demo/widget/check_internet_screen.dart
         return "Không có kết nối";
       default:
         return "Trạng thái không xác định";
-========
-        return "Đang không có kết nối";
-      default:
-        return "Trạng thái không biết";
->>>>>>>> hung-dev:lib/checkinternet.dart
     }
   }
 
@@ -128,7 +91,6 @@ class _CheckInternetState extends State<CheckInternet> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<<< HEAD:lib/src_demo/widget/check_internet_screen.dart
         title: const Text('Kiểm tra kết nối Internet'),
         elevation: 4,
       ),
@@ -138,14 +100,6 @@ class _CheckInternetState extends State<CheckInternet> {
           style: const TextStyle(fontSize: 18),
         ),
       ),
-========
-        title: const Text('No internet builder'),
-        elevation: 4,
-      ),
-      body: Center(
-          child: Text(
-              'Connection Status: ${getConnectStatusMess(_connectionStatus)}')),
->>>>>>>> hung-dev:lib/checkinternet.dart
     );
   }
 }
